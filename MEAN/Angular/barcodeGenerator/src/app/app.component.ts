bef930a9582ed4d0275b77ab81a16e7f13dca2b5
Claudia Viscarra <claudiavisfoss@gmail.com>
2017-06-13 16:41:17 -0400

    barcode generator

---
+++ MEAN/Angular/barcodeGenerator/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Retro Barcode Generator';
+  color = ['DarkSeaGreen', 'Azure', 'AquaMarine', 'red' ,'MediumAquaMarine'];
+  a:number =  (Math.floor(Math.random() * 4)) + 1;
+  b:number =  (Math.floor(Math.random() * 4)) + 1;
+  c:number =  (Math.floor(Math.random() * 4)) + 1;
+  d:number =  (Math.floor(Math.random() * 4)) + 1;
+  e:number =  (Math.floor(Math.random() * 4)) + 1;
+
+}
